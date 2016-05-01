9656fcf27e6b919ba705f9f6755b0cfdcbf83c91
wingleungchoi <wingleungchoi@gmail.com>
2016-05-01 20:24:40 +0800

    task 7

--- program.js
+++ program.js
@@ -71,3 +71,12 @@
 // };
 
 // myModule(pathToDir, extname, readdirCallback);
+
+var http = require('http');
+var url  = process.argv[2];
+
+http.get(url, function(response) {
+  response.setEncoding('utf8')
+  response.on('data', console.log)
+  response.on('error', console.error)
+});
