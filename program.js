fac99cbfa00bc7f186d31ada38ddef9867986351
wingleungchoi <wingleungchoi@gmail.com>
2016-05-01 11:15:10 +0800

    task 5

--- program.js
+++ program.js
@@ -6,20 +6,47 @@
 // var numberOfNewLines = lines.length - 1;
 // console.log(numberOfNewLines);
 
-var fs     = require('fs');
-function logNumberOfNewLines(numberOfNewLines){
-  console.log(numberOfNewLines);
-};
-function countNumberOfNewLines(callback) {
-  fs.readFile(process.argv[2], { encoding: 'utf8' }, function (err, data) {
-    if (err) {
-      throw err;
-    } else{
-      var lines  = data.split('\n');
-      var numberOfNewLines = lines.length - 1;
-      callback(numberOfNewLines);
-    };
-  });
-};
+// task 4
+// var fs     = require('fs');
+// function logNumberOfNewLines(numberOfNewLines){
+//   console.log(numberOfNewLines);
+// };
+// function countNumberOfNewLines(callback) {
+//   fs.readFile(process.argv[2], { encoding: 'utf8' }, function (err, data) {
+//     if (err) {
+//       throw err;
+//     } else{
+//       var lines  = data.split('\n');
+//       var numberOfNewLines = lines.length - 1;
+//       callback(numberOfNewLines);
+//     };
+//   });
+// };
 
-countNumberOfNewLines(logNumberOfNewLines);
+// countNumberOfNewLines(logNumberOfNewLines);
+
+
+
+// task 5
+// var fs         = require ('fs');
+// var path       = require ('path');
+// var pathToDir  = process.argv[2];
+// var extname    = process.argv[3];
+
+// function readdirCallback (err, fileNames) {
+//   if (err) {
+//     throw err;
+//   } else{
+//     for (i = 0; i < fileNames.length; i++) {
+//       if (path.extname(fileNames[i]) == ('.' + extname)) {
+//         console.log(fileNames[i]);
+//       };
+//     };
+//   };
+// };
+
+// function filesWithExtname(path, extname) {
+//   fs.readdir(path, readdirCallback);
+// };
+
+// filesWithExtname(pathToDir,extname);
