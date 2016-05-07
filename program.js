b8ee6267540da9b2b130cc53d5fdce3e0ce6a13f
wingleungchoi <wingleungchoi@gmail.com>
2016-05-07 10:48:02 +0800

    task 12

--- program.js
+++ program.js
@@ -167,3 +167,24 @@
 // // suggested solution: fs.createReadStream(process.argv[3]).pipe(res) 
 // })
 // server.listen(port);
+
+// task 12
+// var port            = process.argv[2];
+// var filePath        = process.argv[3];
+// var http            = require('http');
+// var map             = require('through2-map')
+
+// var server = http.createServer(function (req, res) {
+//   if (req.method !== 'POST') {
+//     return res.end('only POST\n');
+//   };
+
+//   res.writeHead(200, {'Content-Type': 'text/plain'});
+//   req.pipe(map(function (chunk) {
+//    return chunk.toString().toUpperCase()
+//   })).pipe(res)
+// });
+
+// server.listen(port);
+
+
